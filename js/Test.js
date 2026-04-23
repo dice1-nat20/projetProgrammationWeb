5dd778332d9f2a1107d2f37370b31f9299fd1f3c
Clouzeau Ulysse <ulysse.clz33@gmail.com>
2026-04-23 13:18:37 +0200

    almost finito (90%)

--- js/Test.js
+++ js/Test.js
@@ -1,17 +1,48 @@
 let isTraitor = false;
-let numbQuestion = 0;
+let numbQuestion = 1;
+let allQuestions = {};
 
+async function fetchQuestions() {
+    try {
+        const response = await fetch('../js/questions.json');
+        if (!response.ok) throw new Error();
 
-function fetchJSON(url) {}
+        allQuestions = await response.json();
+        initQuestion();
+    } catch (error) {
+        document.getElementById("question").innerText = "Error loading intelligence data...";
+    }
+}
 
-function nextQuestion() {
-    // modifie l'afficahge pour passer à la question suivante
+function initQuestion() {
+    const totalQuestions = Object.keys(allQuestions).length;
+
+    if (numbQuestion > totalQuestions) {
+        showResults();
+        return;
+    }
+
+    document.getElementById("numQuest").innerText = numbQuestion + "/"+ totalQuestions;
+    document.getElementById("question").innerText = allQuestions[numbQuestion];
 }
 
 function answer(reponse) {
-    if (reponse == false) {
+    if (reponse === false) {
         isTraitor = true;
     }
+
     numbQuestion += 1;
-    nextQuestion();
+    initQuestion();
 }
+
+function showResults() {
+    document.querySelector(".test").style.display = "none";
+
+    if (isTraitor) {
+        document.querySelector(".traitor").style.display = "block";
+    } else {
+        document.querySelector(".notTraitor").style.display = "block";
+    }
+}
+
+window.onload = fetchQuestions;
